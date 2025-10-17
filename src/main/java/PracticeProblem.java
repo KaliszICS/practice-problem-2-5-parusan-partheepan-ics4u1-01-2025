ddbc060431dbf9e5f0fde986745f839c4101d71e
parusan-partheepan-ics4u1-01-2025 <parusan.partheepan@student.tdsb.on.ca>
2025-10-17 12:04:45 +0000

    make changes

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,7 +2,38 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
-	}
-
-
+        // Test the insertion sort
+        char[] letters = {'d', 'b', 'a', 'c', 'e'};
+        
+        System.out.print("Before sorting: ");
+        for (char c : letters) {
+            System.out.print(c + " ");
+        }
+        
+        insertionSort(letters);
+        
+        System.out.print("\nAfter sorting: ");
+        for (char c : letters) {
+            System.out.print(c + " ");
+        }
+        // Output: a b c d e
+    }
+    
+    public static void insertionSort(char[] characters) {
+        for (int i = 1; i < characters.length; i++) {
+            char current = characters[i];
+            int j = i - 1;
+            
+            // Shift elements greater than current to the right
+            while (j >= 0 && characters[j] > current) {
+                characters[j + 1] = characters[j];
+                j--;
+            }
+            
+            // Insert current in correct position
+            characters[j + 1] = current;
+        }
+    }
 }
+
+
